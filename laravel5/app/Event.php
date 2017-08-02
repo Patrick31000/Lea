e17724d86e57e21be5792ca2303ecdbe9bcd67e2
PatTaylor <patrick.taylor31@gmail.com>
2017-08-02 15:54:53 +0200

    Mise en place table pivot

--- laravel5/app/Event.php
+++ laravel5/app/Event.php
@@ -9,5 +9,9 @@ class Event extends Model
     protected $table = 'events'; // you may change this to your name table
     public $timestamps = true; // set true if you are using created_at and updated_at
     protected $primaryKey = 'id'; // the default is id
+
+    public function users(){
+        return $this->belongsToMany('App\User');
+    }
  
 }
