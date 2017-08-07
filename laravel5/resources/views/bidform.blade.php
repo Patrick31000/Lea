06ef73d8ed23f03df158e71796a52bb03c6c6794
PatTaylor <patrick.taylor31@gmail.com>
2017-08-07 11:02:46 +0200

    Chrono OK

--- laravel5/resources/views/bidform.blade.php
+++ laravel5/resources/views/bidform.blade.php
@@ -30,9 +30,52 @@
                                     <span class="input-group-addon">£</span>
                                     <input title="" class="form-control" type="number" v-model="bidAmount">
                                 </div>
+                                 <script language="JavaScript">
+                
+                function t()
+                {
+            var compteur=document.getElementById('compteur');
+            s=duree;
+            m=0;h=0;
+            if(s<0)
+                        {
+                                compteur.innerHTML="Enchère terminée !<br/>"
+            }
+                        else
+                        {
+                                if(s>59)
+                                {
+                                        m=Math.floor(s/60);
+                                        s=s-m*60
+                }
+                                if(m>59)
+                                {
+                                        h=Math.floor(m/60);
+                    m=m-h*60
+                                }
+                if(s<10)
+                                {
+                                        s="0"+s
+                }
+                if(m<10)
+                                {
+                    m="0"+m
+                }
+                  compteur.innerHTML=h+":"+m+":"+s+"<br>"
+            }
+            duree=duree-1;
+            window.setTimeout("t();",999);
+
+        }
+                
+                </script>
                                 <div class="col-sm-4" id="compterebours">Compte à rebours avant enchères :
-                                    <br><div id="chrono">HH:MM</div>
+                                    <br><div id="compteur"></div>
                                 </div>
+                                <script language="JavaScript">
+                        duree="120";
+                        t();
+                </script>
                             </div>
                         </div>
                         <div class="col-sm-2">
